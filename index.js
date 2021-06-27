d677430e4ac3280fc7ffdadea1077341d832a1eb
topodohugo <86541345+topodohugo@users.noreply.github.com>
2021-06-27 00:44:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http,{
     cors: {
-    origin: '*',
+    origin: 'https://topodos.000webhostapp.com'
     },
     cookie: true
   });
